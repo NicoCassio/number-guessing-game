6177389d113f34b86e7b87ed25347836afdd0278
NicoCassio <niccassantos@gmail.com>
2025-05-14 14:15:18 -0300

    Multiple rounds

--- Main.java
+++ Main.java
@@ -7,40 +7,55 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
     Random random = new Random();
-    int numberToGuess = random.nextInt(100) + 1;
 
-    System.out.println("Welcome to the Number Guessing Game!");
-    System.out.println("I'm thinking of a number between 1 and 100.");
-    System.out.println("You have 5 chances to guess the correct number.");
+    while (true) {
+      int numberToGuess = random.nextInt(100) + 1;
 
-    System.out.println("Please select the difficulty level:");
-    System.out.println("1. Easy (10 chances)");
-    System.out.println("2. Medium (5 chances)");
-    System.out.println("3. Hard (3 chances)");
+      System.out.println("Welcome to the Number Guessing Game!");
+      System.out.println("I'm thinking of a number between 1 and 100.");
+      System.out.println("You have 5 chances to guess the correct number.");
 
-    System.out.print("Enter your choice: ");
-    int choice = scanner.nextInt();
+      System.out.println("Please select the difficulty level:");
+      System.out.println("1. Easy (10 chances)");
+      System.out.println("2. Medium (5 chances)");
+      System.out.println("3. Hard (3 chances)");
 
-    String[] difficulties = { "Easy", "Medium", "Hard" };
+      System.out.print("Enter your choice: ");
+      int choice = scanner.nextInt();
 
-    System.out.println("Great! You have selected the " + difficulties[choice - 1] + " difficulty level.");
-    System.out.println("Let's start the game!");
+      String[] difficulties = { "Easy", "Medium", "Hard" };
 
-    int[] chances = { 10, 5, 3 };
+      System.out.println("Great! You have selected the " + difficulties[choice - 1] + " difficulty level.");
+      System.out.println("Let's start the game!");
 
-    for (int i = 0; i < chances[choice - 1]; i++) {
-      System.out.print("Enter your guess: ");
-      int guess = scanner.nextInt();
+      int[] chances = { 10, 5, 3 };
 
-      if (guess == numberToGuess) {
-        String attemptPlural = i > 0 ? "attempts" : "attempt";
-        System.out.println("Congratulations! You guessed the correct number in " + (i + 1) + " " + attemptPlural + ".");
-        break;
+      for (int i = 0; i < chances[choice - 1]; i++) {
+        System.out.print("Enter your guess: ");
+        int guess = scanner.nextInt();
+
+        if (guess == numberToGuess) {
+          String attemptPlural = i > 0 ? "attempts" : "attempt";
+          System.out
+              .println("Congratulations! You guessed the correct number in " + (i + 1) + " " + attemptPlural + ".");
+          break;
+        }
+
+        String comparativeStatement = guess < numberToGuess ? "greater" : "less";
+
+        System.out.println("Incorrect! The number is " + comparativeStatement + " than " + guess + ".");
       }
 
-      String comparativeStatement = guess < numberToGuess ? "greater" : "less";
+      System.out.println("Do you want to play again?");
+      System.out.println("1. Yes");
+      System.out.println("2. No");
 
-      System.out.println("Incorrect! The number is " + comparativeStatement + " than " + guess + ".");
+      System.out.print("Enter your choice: ");
+      boolean again = scanner.nextInt() == 1 ? true : false;
+
+      if (!again) {
+        break;
+      }
     }
 
     scanner.close();
